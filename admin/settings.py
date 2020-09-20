b715477295a35a726a1cb4e59fc2e4a9e5ec0f62
Dácio Lima <daciolima@MacBook-Pro.local>
2020-09-20 00:26:25 -0300

    Instalaçao do lib whitenoise e conf no arquivo settings.py

--- admin/settings.py
+++ admin/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'apps.base',
 ]
 
@@ -137,9 +138,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# Conf para o Gunicorn servir staticfiles
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #
 # AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
