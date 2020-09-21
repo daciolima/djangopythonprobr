1437ffb25e997543f04471fd020ca1023ce93c1e
Dácio Lima <daciolima@MacBook-Pro.local>
2020-09-21 01:45:10 -0300

    correção conf whitenoise3

--- admin/settings.py
+++ admin/settings.py
@@ -138,6 +138,10 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 # Conf para o Gunicorn servir staticfiles
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
