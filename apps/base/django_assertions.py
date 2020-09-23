52286a73e8ac70a6955d572d74ff55ec286759eb
Dácio Lima <daciolima@MacBook-Pro.local>
2020-09-23 12:21:27 -0300

    Criaçao do módulo django_assertions para teste

---
+++ apps/base/django_assertions.py
@@ -0,0 +1,5 @@
+from django.test import TestCase
+
+_test_case = TestCase()
+
+asert_contains = _test_case.assertContains
--- apps/base/tests/test_home.py
+++ apps/base/tests/test_home.py
@@ -1,6 +1,14 @@
 from django.test import Client
 
+from apps.base.django_assertions import asert_contains
+
 
 def test_status_code(client: Client):
     resp = client.get('/')
     assert resp.status_code == 200
+
+
+def test_title_page(client: Client):
+    resp = client.get('/')
+    asert_contains(resp, '<title>Dácio Lima Dev</title>')
+
