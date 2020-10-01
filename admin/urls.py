1741e6f1b07fe1e6a662da1137246f00d7289b7a
Dácio Lima <daciolima@MacBook-Pro.local>
2020-10-01 17:03:40 -0300

    app video e conf de templates

--- admin/urls.py
+++ admin/urls.py
@@ -2,11 +2,13 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from apps.base import urls as base_urls
+from apps.aperitivo import urls as aperitivo_urls
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(base_urls), name='home')
+    path('', include(base_urls), name='home'),
+    path('aperitivo/', include(aperitivo_urls), name='video')
 ]
 
 if settings.DEBUG:
---
+++ apps/aperitivo/tests.py
@@ -0,0 +1,22 @@
+import pytest
+from django.urls import reverse
+from apps.base.django_assertions import assert_contains
+
+
+@pytest.fixture()
+def resp(client):
+    return client.get(reverse('aperitivo:video', args=('motivacao',)))
+
+
+def test_tatus_code(resp):
+    assert resp.status_code == 200
+
+
+def test_titulo_video(resp):
+    assert_contains(resp, '<h1>Vídeo Aperitivo - Motivação</h1>')
+
+
+def test_conteudo_video(resp):
+    assert_contains(resp, '<iframe width="560" height="315" src="https://www.youtube.com/embed/')
+
+
