a8b9f745fe5e278781dc872e14f493e0c65538b3
Dácio Lima <daciolima@MacBook-Pro.local>
2020-10-09 20:54:21 -0300

    Code videos via slug close#47

--- apps/aperitivo/views.py
+++ apps/aperitivo/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video = {
-        'titulo': 'Video Aperitivo: Motivação',
-        'youtube_id': 'BwAF7ke7Px0'
+    midias = {
+        'motivacao': {'titulo': 'Luciano Ramalho OO em Python', 'youtube_id': 'BwAF7ke7Px0'},
+        'instalacao-windows': {'titulo': 'Python Set Pratice - Conjuntos', 'youtube_id': 'euSpcIikBrw'}
     }
-    return render(request, 'aperitivo/video.html', context={'video': video})
+
+    midia = midias[slug]
+    return render(request, 'aperitivo/video.html', context={'video': midia})
